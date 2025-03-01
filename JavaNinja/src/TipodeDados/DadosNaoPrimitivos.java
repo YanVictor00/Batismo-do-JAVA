2879cb87093c8e9eb7912eb4f0ef6c5f5e6b95ad
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-02-28 21:48:22 -0300

    Dados não primitivos
    Dados que podem obter métodos

--- JavaNinja/src/TipodeDados/DadosNaoPrimitivos.java
+++ JavaNinja/src/TipodeDados/DadosNaoPrimitivos.java
@@ -3,6 +3,16 @@ package TipodeDados;
 public class DadosNaoPrimitivos {
     public static void main(String[] args) {
 
-        
+        /*
+        *   DADOS NÃO PRIMITIVOS: String, Array, Class, enum
+        */
+
+        String name = "Sasuke Uchiha";
+        System.out.println(name.toUpperCase());
+
+        String name2 = "Sakura Onegai KKAKA";
+        String name2lower = name2.toLowerCase();
+        System.out.println(name2lower);
+
     }
 }
