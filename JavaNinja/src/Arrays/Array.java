4f9ef86211fb28149140a2cf97100c4696320f0b
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-03-02 19:23:41 -0300

    Mais Arrays

--- JavaNinja/src/Arrays/Array.java
+++ JavaNinja/src/Arrays/Array.java
@@ -6,25 +6,34 @@ public class Array {
     public static void main(String[] args) {
 
         /*
-        * Array =
+        * Array = DADOS QUE ARMAZENA VARIOS VALORES EM UM UNICO ESPAÇO NA MEMORIA
         */
 
-        String[] ninja = new String[4];
-
+        String[] ninja = new String[5]; //Array String se inicializa com " NULL "
         ninja[0] = "Sasuke Uchiha";
         ninja[1] = "Naruto Uzumaki";
         ninja[2] = "Sakura Haruno";
         ninja[3] = "Kakashi Hatake";
+        ninja[4] = "Hinata Hyuga";
+        System.out.println(ninja[0]);
 
-        System.out.println(Arrays.toString(ninja));
 
-        int[] idade = new int[4];
+        int[] idade = new int[4]; //Array INT se inicializa com " 0 "
         idade[0] = 18;
         idade[1] = 18;
         idade[2] = 18;
         idade[3] = 26;
+        System.out.println(idade[0]);
 
-        System.out.println(Arrays.toString(idade));
+
+        boolean[] vOUf = new boolean[2]; //Array BOLLEAN se inicializa com " FALSE "
+        vOUf[0] = true;
+        System.out.println(vOUf[1]);
+
+
+        double[] doubleV = new double[2]; //Array DOUBLE se inicializa com " 0.0 "
+        doubleV[0] = 0.1f ;
+        System.out.println(doubleV[1]);
 
 
 
