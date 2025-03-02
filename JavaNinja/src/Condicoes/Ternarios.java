345602c15abfb554d532548ad98c077c101158b0
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-03-02 16:33:35 -0300

    Ternarios

---
+++ JavaNinja/src/Condicoes/Ternarios.java
@@ -0,0 +1,19 @@
+package Condicoes;
+
+public class Ternarios {
+    public static void main(String[] args) {
+
+        /*
+         * Ternários = São maneiras de reduzir o código
+         *
+         * Variável = ( Condição) ? Condição verdadera : Condição Falsa;
+         */
+
+        short numeroDeMissoes = 1;
+        String nivel;
+
+        nivel = (numeroDeMissoes >= 10) ? "VOCÊ TEM MAIS DE 10 MISSOES" : "VOCÊ TEM MENOS DE 10 MISSOES";
+        System.out.println(nivel);
+
+    }
+}
