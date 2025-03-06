fa0791d78e9de5ac358b7ac471161286e26a9e1d
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-03-05 21:26:31 -0300

    Calculadora
    Calculado no terminal do Intellij, usando objetos e parâmentros.

---
+++ JavaNinja/src/Testes/Calculadora.java
@@ -0,0 +1,18 @@
+package Testes;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    //objetos da soma
+        int primeiro;
+        int segundo;
+
+
+
+    //método com o returno da soma
+
+    public int Soma(int n1, int n2){
+        return n1 + n2;
+    }
+}
