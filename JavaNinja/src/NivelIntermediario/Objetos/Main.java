faa4533348a1e71967c99456236d9f511d70d091
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-03-05 21:07:16 -0300

    Parametros e Metodos

--- JavaNinja/src/NivelIntermediario/Objetos/Main.java
+++ JavaNinja/src/NivelIntermediario/Objetos/Main.java
@@ -3,15 +3,32 @@ package NivelIntermediario.Objetos;
 public class Main {
     public static void main(String[] args) {
 
+        //Criando Objetos
+
         Ninja naruto = new Ninja();
 
         naruto.nome = "Naruto Uzumaki";
         naruto.idade = 18;
         naruto.aldeia = "Aldeia da folha";
 
-        System.out.println("Nome: " + naruto.nome);
-        System.out.println("Idade: " + naruto.idade);
-        System.out.println("Aldeia: " + naruto.aldeia);
+        Ninja sasuke = new Ninja();
+        sasuke.nome = "Sasuke Uchiha";
+        sasuke.idade = 19;
+        sasuke.aldeia = "Aldeia da folha";
+
+        //Aplicando Métodos aos objetos
+
+       int quantoTempoFalta = sasuke.QuantosAnosFaltaParaSeTornarHokage(50);
+        System.out.println("Falta " + quantoTempoFalta + " Anos para se tornar Hokage.");
+
+        sasuke.SharingaAtivado();
+
+        String chamandoMetodo = sasuke.BemVindo();
+        System.out.println(chamandoMetodo);
+
+
+
+
 
     }
 }
--- JavaNinja/src/NivelIntermediario/Objetos/Ninja.java
+++ JavaNinja/src/NivelIntermediario/Objetos/Ninja.java
@@ -6,4 +6,19 @@ public class Ninja {
     int idade;
     String aldeia;
 
+    // Método void = Método que não tem retorno.
+    public void SharingaAtivado(){
+        System.out.println("O Sharingan ativou!");
+    }
+
+
+    //Método return = Método que precisa de um retorno da tipagem do método, EX: String, int...
+    public String BemVindo(){
+        return "Bom dia! Seja bem vindo.";
+    }
+
+    public int QuantosAnosFaltaParaSeTornarHokage (int idadeMinima){
+        return idadeMinima - idade;
+    }
+
 }
