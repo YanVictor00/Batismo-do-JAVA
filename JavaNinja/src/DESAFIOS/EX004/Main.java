8ee4cedb0e0f8b456d5835891519396b0bd8048b
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-03-11 20:18:03 -0300

    Muito burro ksks

--- JavaNinja/src/DESAFIOS/EX004/Main.java
+++ JavaNinja/src/DESAFIOS/EX004/Main.java
@@ -3,7 +3,6 @@ package DESAFIOS.EX004;
 public class Main {
     public static void main(String[] args) {
 
-        TipoHabilidade h1 = TipoHabilidade.TAIJUTSU;
 
         NinjaBasico narutinho = new NinjaBasico();
         narutinho.nome = "Naruto Uzumak";
@@ -13,7 +12,7 @@ public class Main {
 
         System.out.println("============================");
 
-        NinjaAvacado sasukinho = new NinjaAvacado("Rinnegan: Amenotejikara (ninjutsu espaço-temporal)", h1.type);
+        NinjaAvacado sasukinho = new NinjaAvacado("Rinnegan: Amenotejikara (ninjutsu espaço-temporal)", TipoHabilidade.TAIJUTSU.type);
         sasukinho.nome = "Sasuke Uchiha";
         sasukinho.idade = 19;
         sasukinho.MostrarInformacoes();
