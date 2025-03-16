01d5cf80aaafc9bb0cb376fb1b7c257029773f27
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-03-16 18:10:58 -0300

    Colocando transferencia
    Ainda não terminei

--- JavaNinja/src/DESAFIOS/EX005/Main.java
+++ JavaNinja/src/DESAFIOS/EX005/Main.java
@@ -6,50 +6,59 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double opcao;
+        int opcao = 0;
+        int depOUtrans;
 
-        System.out.println("========== BANCO KONOHA ==========");
-        System.out.println("1 - Conta Corrente");
-        System.out.println("2 - Conta Poupança");
-        System.out.println("3 - Sair");
-        double saldo = 100;
-
-        opcao = entrada.nextDouble();
 
         while(opcao != 3){
-            if (opcao == 1) {
-                ContaCorrente banco = new ContaCorrente(saldo, TipoDeConta.CORRENTE);
-                System.out.println(banco.tipoConta);
-                System.out.println("Seu saldo é de: R$" + banco.saldo);
-                System.out.println("Deposite um valor: R$");
-                double valor = entrada.nextDouble();
-                System.out.println("Seu deposito foi de R$" + valor);
-                System.out.println( "Seu banco não tem taxa de Depósito");
-                banco.depositar(valor);
-                opcao = 3;
 
-            } else if (opcao == 2) {
-                ContaPoupanca banco = new ContaPoupanca(saldo, TipoDeConta.POUPANÇA);
-                System.out.println(banco.tipoConta);
-                System.out.println("Seu saldo é de: R$" + banco.saldo);
+            System.out.println("========== BANCO KONOHA ==========");
+            System.out.println("1 - Conta Corrente");
+            System.out.println("2 - Conta Poupança");
+            System.out.println("3 - Sair");
+            opcao = entrada.nextInt();
+
+            switch (opcao){
+                case 1 :
+                ContaCorrente ContaCorrente = new ContaCorrente(100, TipoDeConta.CORRENTE);
+                System.out.println(ContaCorrente.tipoConta);
+                System.out.println("Seu saldo é de: R$" + ContaCorrente.saldo);
+
+                System.out.println("ESCOLHA UMA TRANSAÇÃO");
+                System.out.println("1 - DEPOSITAR");
+                System.out.println("2 - ENVIAR PIX");
+                System.out.println("3 - VERIFICAR SALDO");
+                depOUtrans = entrada.nextInt();
+
+                if (depOUtrans == 1){
+                    System.out.println("Deposite um valor: R$");
+                    double valor = entrada.nextDouble();
+                    System.out.println("Seu deposito foi de R$" + valor);
+                    System.out.println( "Seu banco não tem taxa de Depósito");
+                    ContaCorrente.depositar(valor);
+                    ContaCorrente.saldo = ContaCorrente.saldo + valor;
+                }
+
+                break;
+
+                case  2:
+                ContaPoupanca ContaPoupanca = new ContaPoupanca(100, TipoDeConta.POUPANÇA);
+                System.out.println(ContaPoupanca.tipoConta);
+                System.out.println("Seu saldo é de: R$" + ContaPoupanca.saldo);
                 System.out.println("Deposite um valor: R$");
                 double valor = entrada.nextDouble();
                 System.out.println("Seu deposito foi de R$" + valor);
                 System.out.println( "Seu banco cobra uma taxa de 1% a cada deposito");
-                banco.depositar(valor);
-                opcao = 3;
-            }else {
-                System.out.println("ESCOLHA UMA DAS OPÇÕES!");
-                opcao = entrada.nextDouble();
-            }
+                ContaPoupanca.depositar(valor);
 
+
+                default:
+                System.out.println("ESCOLHA UMA DAS OPÇÕES!");
+                opcao = entrada.nextInt();
+            }
 
         }
         System.out.println("Sistema Finalizado.");
-
-
-
-
         entrada.close();
     }
 }
