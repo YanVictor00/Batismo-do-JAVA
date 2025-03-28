ea96401df2323ffaac45f05284112e0b9f8b3485
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-03-28 18:59:53 -0300

    EX006 CHECK

--- JavaNinja/src/DESAFIOS/EX006/Ninja.java
+++ JavaNinja/src/DESAFIOS/EX006/Ninja.java
@@ -3,6 +3,6 @@ package DESAFIOS.EX006;
 public record Ninja(String nome, int idade, String aldeia) {
     @Override
     public String toString() {
-        return  nome + " - " + idade + " - " + aldeia + "]";
+        return  "[ NOME: "+ nome + ", IDADE: " + idade + ", ALDEIA: " + aldeia + " ]\n";
     }
 }
